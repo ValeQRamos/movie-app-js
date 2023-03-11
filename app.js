dc1775bf7258dab05f545be8d4fe0d8f6629ed8a
Vale Quiroga <90653297+ValeQRamos@users.noreply.github.com>
2023-03-11 11:16:57 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+import "regenerator-runtime/runtime";
+import axios from "axios";
+
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=23442aca5a940bca4f679e9303e5a91e&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=23442aca5a940bca4f679e9303e5a91e&query="';
+
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const main = document.getElementById("main");
+
+const getClassByRate = (vote) => {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+};
+
+const showMovies = (movies) => {
+  main.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+
+    movieEl.innerHTML = `
+
+        <img
+          src="${IMG_PATH + poster_path}"
+          alt="${title}"
+        />
+        <div class="movie-info">
+          <h3> ${title} </h3>
+          <span class="${getClassByRate(vote_average)}"> ${vote_average} </span>
+        </div>
+        <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>`;
+
+    main.appendChild(movieEl);
+  });
+};
+
+const getMovies = async (url) => {
+  const { data } = await axios(url);
+  showMovies(data.results);
+};
+getMovies(API_URL);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm);
+
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
